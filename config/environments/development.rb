69725babcca0485efd7d6587059c88b5b9789cb7
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-06-13 08:27:47 +0000

    [FInish]Gemfileに必要なGemを記載

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,9 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.hosts.clear
+
+  config.active_job.queue_adapter = :inline
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Make code changes take effect immediately without server restart.
