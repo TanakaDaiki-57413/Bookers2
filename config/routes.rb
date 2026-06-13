e04cbff62ff1808261aa8508096b04caaddc327c
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-06-13 10:05:54 +0000

    [Finish]books・usersコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get "users/index"
+  get "users/show"
+  get "users/edit"
+  get "books/index"
+  get "books/show"
+  get "books/edit"
   get "homes/top"
   resource :session
   resources :passwords, param: :token
