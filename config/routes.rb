b1030ecce727ee89ed51be6250bdb8c903af5f2d
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-06-13 12:50:34 +0000

    [Update]ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
 
   root to: "homes#top"
   get "/about" => "homes#about"
+  get "/users/sign_in" => "sessions#new" 
 
-  resource :session
+  resource :session, only: [:edit,:show,:update,:destroy,:create]
   resources :passwords, param: :token
 
-  resources :users, only: [:new,:index,:show,:edit] , path_names: { new: 'sign_up' }
+  resources :users, only: [:new,:index,:show,:edit],path_names: { new: 'sign_up' }
   resources :books, only: [:index,:show,:edit]
   
   get "up" => "rails/health#show", as: :rails_health_check
