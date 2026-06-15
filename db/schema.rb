ec9e1d1d085dc4d9eec1c09c788cc6fae1b39bb3
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-06-15 09:36:52 +0000

    [Update]Usersテーブルnameカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_13_093627) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_15_093320) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -61,6 +61,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_13_093627) do
     t.datetime "created_at", null: false
     t.string "email_address", null: false
     t.text "introduction"
+    t.string "name"
     t.string "password_digest", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
