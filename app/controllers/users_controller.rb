884f4b82a114e26830bfd6141bddf25137536288
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-06-15 09:37:24 +0000

    [Finish]Sign up機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   # 認証をスキップ: サインアップ（new, create）はログイン前に行うため
-  allow_unauthenticated_access only: [:new] 
+  allow_unauthenticated_access only: [:new,:create] 
 
   def new
     @user = User.new
@@ -21,5 +21,12 @@ class UsersController < ApplicationController
     # 
     # redirect_to ユーザー詳細ページへ 
     redirect_to root_path
+  end
+
+  private
+ 
+  def user_params
+    # name, email_address, password, password_confirmation を許可
+    params.require(:user).permit(:name, :email_address, :password, :password_confirmation, :profile_image)  
   end
 end
